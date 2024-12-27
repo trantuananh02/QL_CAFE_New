9e984d560676d69222c6799b067bfc84877f1561
trant <trant@STORKTRAN>
2024-12-28 00:50:30 +0700

    hehe

--- QL_CAFE/Program.cs
+++ QL_CAFE/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QL_CAFE.Views;
 
 namespace QL_CAFE
 {
@@ -16,7 +17,7 @@ namespace QL_CAFE
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormDangNhap());
         }
     }
 }
